81fd83cde8b8a37def58201d1ac69cc13395818f
Filip Kobiec <fipis14@gmail.com>
2020-04-23 15:09:40 +0200

    Add handle_click() function

--- Lab2 Kółko i Krzyżyk/src/tic_tac_toe.js
+++ Lab2 Kółko i Krzyżyk/src/tic_tac_toe.js
@@ -1,22 +1,50 @@
 var Board = /** @class */ (function () {
     function Board() {
-        this.play_board = ["", "", "", "", "", "", "", "", ""];
+        this.cells = new Array();
         this.draw_board();
+        for (var i = 0; i < 9; i++) {
+            var htmlCell = document.getElementById("cell_" + (i));
+            var cell = new Cell(this, i, htmlCell);
+            this.cells.push(cell);
+        }
     }
     Board.prototype.draw_board = function () {
         var board = document.querySelector(".board");
         board.innerHTML = '';
-        this.play_board.forEach(function (e, i) {
-            board.innerHTML += "<div id=\"block_" + i + "\" class=\"block\"></div>";
-        });
+        for (var i = 0; i < 9; i++) {
+            board.innerHTML += "<div id=\"cell_" + i + "\" class=\"cell\"></div>";
+        }
+    };
+    Board.prototype.is_win = function () {
+        return true;
     };
     return Board;
 }());
 var Cell = /** @class */ (function () {
-    function Cell() {
+    function Cell(board, index, htmlElement) {
+        var _this = this;
+        this.board = board;
+        this.index = index;
+        this.htmlElement = htmlElement;
+        this.htmlElement.onclick = function () { return _this.handle_click(); };
     }
+    Cell.prototype.handle_click = function () {
+        if (this.htmlElement.innerHTML === "")
+            if (turn === 0)
+                this.htmlElement.innerHTML = "O";
+            else
+                this.htmlElement.innerHTML = "X";
+        switch_turn(turn);
+    };
     return Cell;
 }());
+var turn = 0;
+var switch_turn = function (current_turn) {
+    if (current_turn === 0)
+        turn = 1;
+    else
+        turn = 0;
+};
 window.onload = function () {
     var board = new Board();
 };
--- Lab2 Kółko i Krzyżyk/src/tic_tac_toe.ts
+++ Lab2 Kółko i Krzyżyk/src/tic_tac_toe.ts
@@ -1,23 +1,64 @@
 class Board{
-    play_board = ["", "", "", "", "", "", "", "", ""];
+    private cells: Cell[]
 
     constructor(){
+        this.cells = new Array()
         this.draw_board()
+        for (let i = 0; i < 9; i++){
+            let htmlCell = <HTMLDivElement>document.getElementById("cell_" + (i))
+            let cell = new Cell(this, i, htmlCell)
+            this.cells.push(cell)
+        }
     }
 
     draw_board() {
         const board = document.querySelector(".board")
         board.innerHTML = ''
-        this.play_board.forEach((e, i) => {
-            board.innerHTML += `<div id="block_${i}" class="block"></div>`
-        });
+        for (let i = 0; i < 9; i++){
+            board.innerHTML += `<div id="cell_${i}" class="cell"></div>`
+        }
+    }
+
+
+    is_win(): boolean {
+        return true
     }
 
 }
 
 class Cell{
+    board: Board
+    index: number
+    htmlElement: HTMLDivElement
+    content: string
 
+    constructor(board: Board, index: number, htmlElement: HTMLDivElement) {
+        this.board = board
+        this.index = index
+        this.htmlElement = htmlElement
+        this.htmlElement.onclick = () => this.handle_click()
+    }
+
+    handle_click() {
+        if (this.htmlElement.innerHTML === "") 
+            if (turn === 0)
+                this.htmlElement.innerHTML = "O"
+            else
+                this.htmlElement.innerHTML = "X"
+        
+        switch_turn(turn)
+    }
 }
+
+let turn = 0
+
+const switch_turn = (current_turn: number) => {
+    if (current_turn === 0) 
+        turn = 1
+    else
+        turn = 0
+}
+
 
 window.onload = () => {
     const board = new Board()
