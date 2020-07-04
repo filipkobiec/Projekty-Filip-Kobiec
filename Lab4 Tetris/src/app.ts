99d89d121d53ca1fd1cbf173a204399806df5287
Filip Kobiec <fipis14@gmail.com>
2020-07-04 19:23:04 +0200

    Create basic structure for project

--- Lab4 Tetris/src/app.ts
+++ Lab4 Tetris/src/app.ts
@@ -1,6 +1,58 @@
 export class App {
+    boardWidth: number = 400;
+    boardHeight: number = 800;
     constructor() {
-        // const b = new Form()
+        this.initialize()
+    }
+
+    initialize(){
+        const gameCanvas = new GameCanvas('game', this.boardWidth, this.boardHeight)
     }
 }
-export const AAA = 10;
+
+export class GameCanvas{
+    constructor(containerId: string, width: number, height: number){
+        this.initialize(containerId, width, height);
+    }
+
+    initialize(containerId: string, width: number, height: number) {
+        const canvas: HTMLCanvasElement = document.createElement('canvas');
+        canvas.width = width;
+        canvas.height = height;
+        canvas.id = 'board'
+        const container = document.querySelector('#' + containerId);
+        container.appendChild(canvas);
+        const ctx = canvas.getContext('2d');
+    }
+
+    drawBlock(blockType: BlockType, BlockPosition: BlockPosition, ctx: CanvasRenderingContext2D): void {
+        ctx.fillStyle = '#ff0050';
+        ctx.fillRect(50, 50, 100, 100);
+    }
+
+    drawBoard(boardMatrix: number[][]){
+
+    }
+}
+
+export interface Block{
+    readonly type: BlockType;
+    readonly color: string;
+    position: BlockPosition;
+    isActive: boolean;
+    width: number;
+    height: number;
+    shape: number[][];
+    setPosition(position: Position): void;
+    getPosition(position: Position): Position;
+};
+
+export interface BlockPosition{
+    x: number;
+    y: number;
+}
+
+enum BlockType{
+    square,
+    line
+}
